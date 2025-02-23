296db5040364a0d90d840d3efda42bb54b91c2e4
Vladyslav Fesenko <vladyslav.fesenko.24@gmail.com>
2025-02-23 22:51:17 +0100

    add scanBasePackages

--- src/main/java/com/example/taskplanner/TaskPlannerApplication.java
+++ src/main/java/com/example/taskplanner/TaskPlannerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.taskplanner")
 @EnableJpaRepositories
 public class TaskPlannerApplication {
     public static void main(String[] args) {
